439387784b2d32931ca6bf715d048b3b76223c87
piptouque <piptouque@protonmail.com>
2021-10-04 16:10:31 +0200

    [+]: added .gitignore.

--- utility/computation.py
+++ utility/computation.py
@@ -31,8 +31,9 @@ class TransversalVibration:
     def get_free_vibration(cls, bar: Bar, cs: List[Constraint]) -> Callable[[float], float]:
         def k(omega): return np.sqrt(
             omega) * np.power(bar.mat.rho * bar.shape.s / (bar.mat.e * bar.mat.i), 1/4)
-        phi = cls.__spatial_constant(bar, cs) if bar.shape.is_section_constant(
+        (ds, w_n) = cls.__spatial_constant(bar, cs) if bar.shape.is_section_constant(
         ) else cls.__spatial_variable(bar, cs)
+        return lambda
         pass  # todo
 
     """[summary]
@@ -43,25 +44,32 @@ class TransversalVibration:
     def __spatial_constant(cls, bar: Bar, cs: Tuple[Constraint, Constraint]) -> Tuple[np.ndarray, Callable[[int], float]]:
         couple = Constraint.couples[cs]
         if couple == 'free-free':
+            ds = np.array([1, 1, 1, 1])
+
+            def w_n_3(n):
+                return np.sqrt(bar.mat.e * bar.shape.i / (bar.mat.rho * bar.shape.s)))
+                 * ((2 * n + 1) * np.pi) ** 2 /
+                    (8 * bar.shape.l ** 2) pass
+            return (ds, w_n_3)
             pass
         if couple == 'free-clamped':
-            ds = np.array([1, 1, 0, 0])
+            ds=np.array([1, 1, 0, 0])
 
             def w_n_1(n):
-                return ((2 * n + 1) * np.pi) ** 2 / \
+                return ((2 * n + 1) * np.pi) ** 2 /
                     (4 * bar.shape.l * np.sqrt(bar.mat.e *
                                                bar.shape.i / (bar.mat.rho * bar.shape.s)))
-            return (ds, w_n)
+            return (ds, w_n_1)
         if couple == 'supported-supported':
             # même distance au bord le plus proche pour les positions des conditions limites
             # assert(couple[0].x + couple[1].x == 2)
             # advancement= couple[0].x
             # ne fonctionne pas pour avancement != 0...
-            ds = np.array([0, 0, 1, 0])
+            ds=np.array([0, 0, 1, 0])
 
-            def w_n_1(n):
+            def w_n_2(n):
                 return (np.pi * (2 * n + 1) / (2 * bar.shape.l)) ** 2 * np.sqrt(bar.mat.e * bar.shape.i / (bar.mat.rho * bar.shape.s))
-            return (ds, w_n)
+            return (ds, w_n_2)
 
     @ classmethod
     def __spatial_variable(cls, bar: Bar, cs: Tuple[Constraint, Constraint]) -> Tuple[np.ndarray, Callable[[int], float]]:
@@ -83,12 +91,12 @@ class HertzImpulse:
         # Le rayon de courbure au point de contact :
         # - infini pour la lame
         # - égale au rayon de sa tête pour la baguette
-        k = 1.25 * np.sqrt(mallet.shape.r) / ((1 - bar.mat.nu ** 2) /
+        k=1.25 * np.sqrt(mallet.shape.r) / ((1 - bar.mat.nu ** 2) /
                                               bar.mat.e + (1 - mallet.mat.nu ** 2) / mallet.mat.e)
-        m_1 = bar.get_mass()
-        m_2 = mallet.get_mass()
-        m_r = m_1 * m_2 / (m_1 + m_2)
-        delta_max = np.power((5/4) * m_r * v_0 ** 2 / k, 2/5)
-        tau = 3.218 * np.power(m_r ** 2 / (k ** 2 * v_0), 1/5)
+        m_1=bar.get_mass()
+        m_2=mallet.get_mass()
+        m_r=m_1 * m_2 / (m_1 + m_2)
+        delta_max=np.power((5/4) * m_r * v_0 ** 2 / k, 2/5)
+        tau=3.218 * np.power(m_r ** 2 / (k ** 2 * v_0), 1/5)
 
         return lambda t: k * np.power(delta_max, 1.5) * np.exp(- t / tau)
